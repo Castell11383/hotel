15abca567e9f354350af9115ef8e6be4e2ab449b
CUXUM <mcbrayan20@gmail.com>
2024-09-23 15:29:07 -0600

    subir

--- controllers/InicioController.php
+++ controllers/InicioController.php
@@ -8,7 +8,7 @@ class InicioController {
     public static function index(Router $router){
          $router->render('inicio/index',[],'layouts/menu');
          isAuth();
-         hasPermission(['TIENDA_ADMIN', 'TIENDA_USER']);
+          hasPermission(['TIENDA_ADMIN', 'TIENDA_USER']);
     }
 
 }
